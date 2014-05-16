3de37cec50b5e234f3b88f2d5a41312916738ae2
dinagarrett <butsefala@gmail.com>
2014-05-16 21:40:56 +0400

    eeeee

--- web/index.php
+++ web/index.php
@@ -2,8 +2,16 @@
 // web/index.php
 require_once __DIR__.'/../vendor/autoload.php';
 
+use Silex\Provider\FormServiceProvider;
+
+
+
 $app = new Silex\Application();
-$app['debug'] = true;
+
+$app['debug']=true;
+
+$app->register(new FormServiceProvider());
+
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
     'twig.path' => __DIR__.'/views',
 ));
@@ -18,6 +26,39 @@ $app->register(new Silex\Provider\DoctrineServiceProvider(), array(
             'charset'   => 'utf8',
         ),
 ));
+
+$app->register(new Silex\Provider\ValidatorServiceProvider());
+$app->register(new Silex\Provider\TranslationServiceProvider(), array(
+    'translator.domains' => array(),
+));
+
+$app->get('/', function () use ($app){
+	return $app['twig']->render('index.twig');
+
+
+
+
+});
+
+$app->get('/registration', function () use ($app) {
+
+	$form = $app['form.factory']->createBuilder('form')
+        ->add('name', null, array(
+        	'label' => 'Имя'
+        	))
+        ->add('surname')
+        ->add('byear')
+        ->add('email')
+        ->add('category', 'choice', array(
+            'choices' => array(1 => 'male', 2 => 'female'),
+            'label' => 'Категория'
+            ))
+        ->getForm();
+
+
+    return $app['twig']->render('hello.twig', array('form' => $form->createView()));
+});
+
 
 $app->get('/admin', function () use ($app) {
 	$riders = $app['db']->fetchAll('SELECT * FROM riders');
