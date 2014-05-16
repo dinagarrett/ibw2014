089cfe69ffda784f0b9e2785eb35e61dfc9c2530
Tigress <tigressmda@gmail.com>
2014-05-16 21:25:15 +0400

    admin panel

--- web/index.php
+++ web/index.php
@@ -3,13 +3,27 @@
 require_once __DIR__.'/../vendor/autoload.php';
 
 $app = new Silex\Application();
-
+$app['debug'] = true;
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
     'twig.path' => __DIR__.'/views',
 ));
 
-$app->get('/hello/{name}', function ($name) use ($app) {
-    return $app['twig']->render('hello.twig', array('name' => $name));
-});
+$app->register(new Silex\Provider\DoctrineServiceProvider(), array(
+    'db.options' => array(
+			'driver'    => 'pdo_mysql',
+            'host'      => 'localhost',
+            'dbname'    => 'silex',
+            'user'      => 'root',
+            'password'  => '',
+            'charset'   => 'utf8',
+        ),
+));
 
+$app->get('/admin', function () use ($app) {
+	$riders = $app['db']->fetchAll('SELECT * FROM riders');
+//	var_dump($riders);
+    return $app['twig']->render('admin.twig', array(
+	'riders' => $riders,
+	));
+});
 $app->run();
